3cf0e56203a2247035c4640545d6d897e87e6656
ibnu <62598513+ibnubuild@users.noreply.github.com>
2020-05-04 12:20:31 +0800

    4.3. Difficulty and The Nonce value Code

--- block.test.js
+++ block.test.js
@@ -7,13 +7,17 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
-    const block = new Block ({ timestamp, lastHash, hash, data });
+    const nonce = 1;
+    const difficulty = 1;
+    const block = new Block ({ timestamp, lastHash, hash, data, nonce, difficulty });
 
-    it('has a timestamp, lasthash, hash and data', ()=> {
+    it('has a timestamp, lasthash, hash and data property', ()=> {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     describe('genesis()', () => {
@@ -36,6 +40,7 @@ describe('Block', () => {
         it('return a Block instance', () => {
             expect(minedBlock instanceof Block).toBe(true);
         });
+
         it('sets the lastHash tobe the hash of the lastBlock', () => {
             expect(minedBlock.lastHash).toEqual(lastBlock.hash);
         });
@@ -50,7 +55,19 @@ describe('Block', () => {
 
         it('creates a SHA-256 based on the proper inputs', () => {
             expect(minedBlock.hash)
-                .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+                .toEqual(cryptoHash(
+                    minedBlock.timestamp, 
+                    minedBlock.nonce, 
+                    minedBlock.difficulty, 
+                    lastBlock.hash, 
+                    data
+                )
+            );
+        });
+
+        it('sets a hash that matches the difficulty creteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty));
         });
     });
 });
