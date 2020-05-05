30e0555e16f0d17017efc1c6d585cb5f6d531984
ibnu <62598513+ibnubuild@users.noreply.github.com>
2020-05-05 21:15:25 +0800

    6.4. Create and Test Wallet Class - key pair & public pair address

--- index.js
+++ index.js
@@ -1,54 +1,14 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const request = require('request');
-const Blockchain = require('./blockchain');
-const PubSub = require('./app/pubsub');
+const { STARTING_BALANCE } = require('../config');
+const { ec } = require('../util');
 
-const app = express();
-const blockchain = new Blockchain();
-const pubsub = new PubSub({ blockchain });
+class Wallet {
+    constructor(){
+        this.balance = STARTING_BALANCE;
 
-const DEFAULT_PORT = 3000;
-const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
+        const keyPair = ec.genKeyPair();
 
-app.use(bodyParser.json());
-
-app.get('/api/blocks', (req, res) => {
-    res.json(blockchain.chain);
-});
-
-app.post('/api/mine', (req, res) => {
-    const { data } = req.body;
-
-    blockchain.addBlock({ data });
-
-    pubsub.broadcastChain();
-
-    res.redirect('/api/blocks');
-});
-
-const syncChains = () => {
-    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (error, response, body) => {
-        if(!error && response.statusCode === 200) {
-            const rootChain = JSON.parse(body);
-
-            console.log('replace chain on a sync with', rootChain);
-            blockchain.replaceChain(rootChain);
-        }
-    });
+        this.publicKey = keyPair.getPublic().encode('hex');
+    }
 };
 
-let PEER_PORT;
-
-if (process.env.GENERATE_PEER_PORT === 'true') {
-    PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
-}
-
-const PORT = PEER_PORT || DEFAULT_PORT;
-app.listen(PORT, () => {
-    console.log(`listening at localhost:${PORT}`);
-
-    if (PORT !== DEFAULT_PORT) {
-        syncChains();
-    }
-});
+module.exports = Wallet;
