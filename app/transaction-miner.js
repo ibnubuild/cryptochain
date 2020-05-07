e5e38d61f6ec0e7bf874bbe79442304f879ddf2e
ibnu <62598513+ibnubuild@users.noreply.github.com>
2020-05-07 12:25:24 +0800

    8.6. Mine Transaction Endpoint

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
     constructor({ blockchain, transactionPool, wallet, pubsub }) {
         this.blockchain = blockchain;
@@ -5,14 +7,23 @@ class TransactionMiner {
         this.wallet = wallet;
         this.pubsub = pubsub;
     }
-    mineTransactions() {}
+    mineTransactions() {
     // get the transaction pool's valid transactions
+    const validTransactions = this.transactionPool.validTransactions();
 
     // generate the miner's reward
+    validTransactions.push(
+    Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
 
     // add a block consisting of these transaction to the blockchain
+    this.blockchain.addBlock({ data: validTransactions });
+    
+    this.pubsub.broadcastChain();
 
     // clear the pool
+    this.transactionPool.clear();
+    }
 }
 
 module.exports = TransactionMiner;
