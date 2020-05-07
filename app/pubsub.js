7f41c8b4cc61c5ec84df17b1cdb84e5019b9abc5
ibnu <62598513+ibnubuild@users.noreply.github.com>
2020-05-07 12:54:37 +0800

    8.7. Clear Recorded Transactions on Successful Replace
    post http://localhost:3000/api/transact
    get http://localhost:3000/api/mine-transactions
    get http://localhost:3000/api/transaction-pool-map

--- app/pubsub.js
+++ app/pubsub.js
@@ -55,7 +55,11 @@ class PubSub {
 
                 switch(channel) {
                     case CHANNELS.BLOCKCHAIN:
-                        this.blockchain.replaceChain(parsedMessage);
+                        this.blockchain.replaceChain(parsedMessage, () => {
+                            this.transactionPool.clearBlockchainTransactions({
+                                chain: parsedMessage
+                            })
+                        });
                     break;
                     case CHANNELS.TRANSACTION:
                         if(!this.transactionPool.existingTransaction({
